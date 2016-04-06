643ed72a9bf7d947aa3120107e3cbc9b35c6fdcb
jared beckler and torrence stratton <tlstratton@gmail.com>
2016-04-06 10:19:40 -0700

    add display for amount of items in cart

--- app/components/cart-length.js
+++ app/components/cart-length.js
@@ -2,8 +2,4 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   shoppingCart: Ember.inject.service(),
-  cartTotal: Ember.computed('product.price', function() {
-    return this.get('product.price');
-  }),
-
 });
