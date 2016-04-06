e9ef6dd5d6dc7ee7172a8c6aa81f9eaf61a45e77
jared beckler and torrence stratton <jared.beckler@gmail.com>
2016-04-06 16:08:19 -0700

    add display for reviews

--- app/routes/item.js
+++ app/routes/item.js
@@ -1,6 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model(params){
+    return this.store.findRecord('item', params.item_id);
+  },
+
   actions: {
     saveReview(params) {
       var newReview = this.store.createRecord('review', params);
