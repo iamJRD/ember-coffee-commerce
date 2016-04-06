ebc0a238a0a4d783786a4b7fa58c3cabc482100f
jared beckler and torrence stratton <jared.beckler@gmail.com>
2016-04-06 15:25:24 -0700

    add functionality for adding a review to an item

---
+++ app/components/new-review.js
@@ -0,0 +1,22 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  addReviewForm: false,
+
+  actions: {
+    addReviewForm() {
+      this.set('addReviewForm', true);
+    },
+
+    saveReview() {
+      var params = {
+        username: this.get('username'),
+        date: this.get('date'),
+        rating: this.get('rating'),
+        body: this.get('body'),
+        item: this.get('item')
+      };
+      this.sendAction('saveReview', params);
+    }
+  }
+});
--- app/routes/item.js
+++ app/routes/item.js
@@ -1,5 +1,16 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  
+  actions: {
+    saveReview(params) {
+      var newReview = this.store.createRecord('review', params);
+      var item = params.item;
+
+      item.get('reviews').addObject(newReview);
+      newReview.save().then(function() {
+        return item.save();
+      });
+      this.transitionTo('item');
+    }
+  }
 });
