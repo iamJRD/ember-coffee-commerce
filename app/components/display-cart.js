c5fd9c6e966fecea3b1270eae8d3e9f0a1db1676
jared beckler and torrence stratton <tlstratton@gmail.com>
2016-04-06 11:08:48 -0700

    add ability to calculate cart total

--- app/components/display-cart.js
+++ app/components/display-cart.js
@@ -2,8 +2,18 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   shoppingCart: Ember.inject.service(),
-  cartTotal: Ember.computed('product.price', function() {
-    return this.get('product.price');
-  }),
-
+  cartTotal: Ember.computed('shoppingCart.products', function() {
+      var total = 0;
+      for(var i = 0; i < this.get('shoppingCart.products.length'); i++) {
+        var price = this.get('shoppingCart.products')[i].get('price');
+        total += price;
+      }
+      return total;
+  })
 });
+
+
+
+
+// WILL NOT WORK FOR SOME REASON. SEEMS LIKE IT SHOULD
+// cartTotal: Ember.computed.sum('shoppingCart.products.@each.price'),
