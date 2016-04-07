64ed3a61e22d8def3e15af19d228070de2f8dd19
jared beckler and torrence stratton <tlstratton@gmail.com>
2016-04-07 11:19:57 -0700

    add destroyreview component to project

--- app/components/destroy-review.js
+++ app/components/destroy-review.js
@@ -2,10 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   actions: {
-    editReview(review, params) {
-      this.sendAction('editReview', review, params);
-    },
-    destroyReview(review){
+    destroyReview(review) {
       this.sendAction('destroyReview', review);
     }
   }
